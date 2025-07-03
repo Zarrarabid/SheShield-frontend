4778d5c3b1291c41e7546d5593f5c75e2b067259
Zarrar Hussain Abid <93248557+Zarrarabid@users.noreply.github.com>
2025-07-03 10:08:26 +0500

    Update config.js

--- config.js
+++ config.js
@@ -11,7 +11,7 @@
 
 const config = {
     API_URL: 'http://192.168.18.48:5000', // Replace with your backend URL
-    GoolgelMap_API: 'AIzaSyCJBRGX_q__nNv1Wfr50kS1oew1RI-gBNE', // Replace with your backend URL
+    GoolgelMap_API: YOUR_API_KEY, // Replace with your backend URL
 };
 
 
